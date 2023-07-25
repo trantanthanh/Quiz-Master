ebd9b45d21a2ddaf1df4bd1ec7a7f871d3015e26
trantanthanh <trantanthanh@gmail.com>
2023-07-26 01:41:20 +0700

    re-arrange font , script folder, Prefabs, CreateAssetMenu

---
+++ Assets/Scripts/QuestionSO.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Question", menuName = "Quiz Question")]
+public class QuestionSO : ScriptableObject
+{
+    
+}
