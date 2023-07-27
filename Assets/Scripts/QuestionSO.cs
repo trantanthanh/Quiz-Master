1dd5d7bc0a0bdae9196f89624ad76cd030641506
trantanthanh <trantanthanh@gmail.com>
2023-07-28 01:11:47 +0700

    add ScriptAbleOject question

--- Assets/Scripts/QuestionSO.cs
+++ Assets/Scripts/QuestionSO.cs
@@ -5,5 +5,14 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Question", menuName = "Quiz Question")]
 public class QuestionSO : ScriptableObject
 {
-    
+    [TextArea(2, 6)]
+    [SerializeField] string question = "Enter your question here";
+
+    [SerializeField] string[] answer = new string[4];
+    [SerializeField] int correctAnswerIndex = 0;
+
+    public string GetQuestion()
+    {
+        return question;
+    }
 }
