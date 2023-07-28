c861d3de97e43bf71b68971609d55faebb077ae3
trantanthanh <trantanthanh@gmail.com>
2023-07-28 15:18:19 +0700

    add 10 questions, and Quiz class to manager quiz canvas

--- Assets/Scripts/QuestionSO.cs
+++ Assets/Scripts/QuestionSO.cs
@@ -8,11 +8,21 @@ public class QuestionSO : ScriptableObject
     [TextArea(2, 6)]
     [SerializeField] string question = "Enter your question here";
 
-    [SerializeField] string[] answer = new string[4];
+    [SerializeField] string[] answers = new string[4];
     [SerializeField] int correctAnswerIndex = 0;
 
     public string GetQuestion()
     {
         return question;
+    }
+
+    public string GetAnswer(int index)
+    {
+        return answers[index];
+    }
+
+    public int GetCorrectAnswerIndex()
+    {
+        return correctAnswerIndex;
     }
 }
---
+++ Assets/Scripts/Quiz.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class Quiz : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI questionText;
+    [SerializeField] QuestionSO question;
+    [SerializeField] GameObject[] answerButtons;
+    // Start is called before the first frame update
+    void Start()
+    {
+        questionText.text = question.GetQuestion();
+        for (int i = 0; i < answerButtons.Length; ++i){
+            TextMeshProUGUI buttonText = answerButtons[i].GetComponentInChildren<TextMeshProUGUI>();
+            buttonText.text = question.GetAnswer(i);
+        }
+    }
+}
