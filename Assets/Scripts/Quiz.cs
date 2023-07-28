25cf3a09d744035c67c9e2a3e298ba1fe9f31eb6
trantanthanh <trantanthanh@gmail.com>
2023-07-28 17:24:08 +0700

    add change image sprite button

--- Assets/Scripts/Quiz.cs
+++ Assets/Scripts/Quiz.cs
@@ -1,19 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 public class Quiz : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI questionText;
     [SerializeField] QuestionSO question;
     [SerializeField] GameObject[] answerButtons;
+
+    int correctAnswerIndex;
+    [SerializeField] Sprite defaultAnswerSprite;
+    [SerializeField] Sprite correctAnswerSprite;
     // Start is called before the first frame update
     void Start()
     {
         questionText.text = question.GetQuestion();
-        for (int i = 0; i < answerButtons.Length; ++i){
+        for (int i = 0; i < answerButtons.Length; ++i)
+        {
             TextMeshProUGUI buttonText = answerButtons[i].GetComponentInChildren<TextMeshProUGUI>();
             buttonText.text = question.GetAnswer(i);
         }
+    }
+
+    public void OnAnswerSelected(int index)
+    {
+        correctAnswerIndex = question.GetCorrectAnswerIndex();
+        if (index == correctAnswerIndex)
+        {
+            questionText.text = "Correct!";
+        }
+        else
+        {
+            questionText.text = "Wrong Answer!. The correct answer is :\n" + question.GetAnswer(correctAnswerIndex);
+        }
+        Image buttonImage = answerButtons[correctAnswerIndex].GetComponent<Image>();
+        buttonImage.sprite = correctAnswerSprite;
     }
 }
